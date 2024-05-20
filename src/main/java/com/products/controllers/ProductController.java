8c1756a6c422a3483cd74df743e04b66462357ec
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-20 14:52:25 -0300

    Recuperando produto pelo id

--- src/main/java/com/products/controllers/ProductController.java
+++ src/main/java/com/products/controllers/ProductController.java
@@ -5,9 +5,13 @@ import com.products.dto.ProductResponseDTO;
 import com.products.models.Product;
 import com.products.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("products")
@@ -28,4 +32,15 @@ public class ProductController {
         List<ProductResponseDTO> productList = productRepository.findAll().stream().map(ProductResponseDTO::new).toList();
         return productList;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProduct(@PathVariable(value = "id") UUID id){
+        Optional<Product> productOptional = productRepository.findById(id);
+        if(productOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not Found");
+        }
+        ProductResponseDTO product = new ProductResponseDTO(productOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
 }
