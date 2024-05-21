d33ad3a67318f26e0c2d9ed1a7d266e0b6c839bf
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-21 13:12:56 -0300

    Adicionando repositório de usuário e serviço de autorização

---
+++ src/main/java/com/products/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.products.repositories;
+
+import com.products.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByUsername(String username);
+}
---
+++ src/main/java/com/products/services/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.products.services;
+
+
+import com.products.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
